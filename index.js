f3bbe72ae07089cdafc77152cb8ac7c8cd293c76
Brendan Le Galo <le.galo.brendan@gmail.com>
2023-04-14 17:52:07 +0200

    Add of js code to say who is the winner

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ var img2 = "images/dice" + n2 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src",img1);
 document.querySelectorAll("img")[1].setAttribute("src",img2);
+
+
+if (n1 > n2){
+    document.querySelector("h1").textContent="Player 1 wins!";
+}
+else if (n1 === n2){
+    document.querySelector("h1").textContent="Draw!";
+}
+else {
+    document.querySelector("h1").textContent="Player 2 wins!";
+}
