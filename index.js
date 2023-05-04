6822768b020e87c294e49c36dc0dd402d8e222bf
Brendan Le Galo <le.galo.brendan@gmail.com>
2023-05-04 19:14:39 +0200

    test

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
-var n1 = Math.ceil(Math.random()*6);
-var n2 = Math.ceil(Math.random()*6);
-console.log(n1);
-console.log(n2);
+// var n1 = Math.ceil(Math.random()*6);
+// var n2 = Math.ceil(Math.random()*6);
+// console.log(n1);
+// console.log(n2);
 
-var img1 = "images/dice" + n1 + ".png";
-var img2 = "images/dice" + n2 + ".png";
+// var img1 = "images/dice" + n1 + ".png";
+// var img2 = "images/dice" + n2 + ".png";
 
-document.querySelectorAll("img")[0].setAttribute("src",img1);
-document.querySelectorAll("img")[1].setAttribute("src",img2);
-
-
-if (n1 > n2){
-    document.querySelector("h1").textContent="Player 1 wins!";
-}
-else if (n1 === n2){
-    document.querySelector("h1").textContent="Draw!!!";
-}
-else {
-    document.querySelector("h1").textContent="Player 2 wins!";
-}
+// document.querySelectorAll("img")[0].setAttribute("src",img1);
+// document.querySelectorAll("img")[1].setAttribute("src",img2);
 
 
+// if (n1 > n2){
+//     document.querySelector("h1").textContent="Player 1 wins!";
+// }
+// else if (n1 === n2){
+//     document.querySelector("h1").textContent="Draw!!!";
+// }
+// else {
+//     document.querySelector("h1").textContent="Player 2 wins!";
+// }
 
 
-document.querySelector(".btn").addEventListener("click",function(){
-    location.reload();
-})
+
+
+// document.querySelector(".btn").addEventListener("click",function(){
+//     location.reload();
+// })
